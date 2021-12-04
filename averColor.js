88a6a853366b0ece50a3a5f009e534df80d56b48
Ilya Aksenov <ilya@aksenov.ws>
2021-12-04 16:54:45 +0700

    init getAverageRGB

--- averColor.js
+++ averColor.js
@@ -1,14 +1,26 @@
 window.addEventListener("load",function(){
 	var picture=document.querySelector("#cover");
-	console(picture);
+	//console.log(picture);
+	makeShadow(picture);
 });
 
 function getAverageRGB(cover)
 {
+	var rgb = {r:0, g:0, b:0},
+			canvas=document.createElement('canvas'),
+			ctx=canvas.getContext('2d'),
+			width,
+			height;
+	
+	width=canvas.width=cover.offsetWidth;
+	height=canvas.height=cover.offsetHeight;
+
+	console.log(width, height);
+
 
 }
 
 function makeShadow(picture)
 {
-
+	var rgb=getAverageRGB(picture);
 }
