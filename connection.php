bc88fa906d25efbe89be8601bb1f0a980064e00e
Ilya Aksenov <ilya@aksenov.ws>
2022-01-03 17:05:31 +0700

    connection

---
+++ connection.php
@@ -0,0 +1,10 @@
+<?php
+	$HOST="localhost";
+	$USER="labweb";
+	$PASSWORD="Uv4UgT2vfnAnQdYb";
+	$DB="labwebdb";
+
+	$connection=new mysqli ($HOST, $USER, $PASSWORD, $DB);
+
+	//$connection -> query ('SET NAMES "utf8"');
+?>
